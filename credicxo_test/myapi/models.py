3a901197b3fa5a8634005ad2e805a3fa40b22d6d
Arnav P R Vashishta <avashishta5@gmail.com>
2020-10-07 05:13:17 +0530

    updated serializer

--- credicxo_test/myapi/models.py
+++ credicxo_test/myapi/models.py
@@ -10,12 +10,15 @@ class Student(model.Model):
 		('IV', 'Fourth'),
 		)
 	name = models.CharField(max_length=100)
-	registration_no = models.CharField(primary_key=True, unique=True)
 	dob = models.DateField()
 	year = models.CharField(max_length=3, choices=YEARS)
 	branch = models.CharField(max_length=3, choices=settings.BRANCHES)
 	guardian_name = models.CharField(max_length=100)
 	phone_no = models.CharField(max_length=10)
+
+	@property
+	def registration_no(self):
+		return "S%05d" % self.id
 
 
 	def __str__(self):
@@ -23,10 +26,14 @@ class Student(model.Model):
 
 class Teacher(model.Model):
 	name = models.CharField(max_length=100)
-	registration_no = models.CharField(primary_key=True, unique=True)
 	dob = models.DateField()
 	branch = models.CharField(max_length=3, choices=settings.BRANCHES)
 	phone_no = models.CharField(max_length=10)
+
+	@property
+	def registration_no(self):
+		return "T%05d" % self.id
+	
 
 	def __str__(self):
 		return self.name
--- credicxo_test/myapi/serializers.py
+++ credicxo_test/myapi/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from .models import Student, Teacher
 
+
 class StudentSerializer(serializers.HyperlinkedModelSerializer):
+	registration_no = serializers.ReadOnlyField()
+
 	class Meta:
 		model = Student
-		fields = ('name', 'dob', 'year', 'branch', 'guardian_name', 'phone_no')
+		fields = "__all__"
 
 class TeacherSerializer(serializers.HyperlinkedModelSerializer):
+	registration_no = serializers.ReadOnlyField()
+	
 	class Meta:
 		model = Teacher
-		fields = ('name', 'dob', 'branch', 'phone_no')
+		fields = "__all__"
