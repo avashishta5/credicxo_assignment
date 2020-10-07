385a16fbf56a8a903e4f43602aa18792c5d4f933
Arnav P R Vashishta <avashishta5@gmail.com>
2020-10-07 22:40:31 +0530

    fixed models, updated serializers

--- credicxo_test/myapi/models.py
+++ credicxo_test/myapi/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.conf import settings
 
-# Create your models here.
 class Student(models.Model):
 	YEARS = (
 		('I', 'First'),
@@ -10,30 +11,37 @@ class Student(models.Model):
 		('IV', 'Fourth'),
 		)
 	name = models.CharField(max_length=100)
+	registration_no = models.CharField(max_length=6, null=True, unique=True)
 	dob = models.DateField()
-	year = models.CharField(max_length=3, choices=YEARS)
-	branch = models.CharField(max_length=3, choices=settings.BRANCHES)
+	year = models.CharField(max_length=3, choices=YEARS, default=YEARS[0])
+	branch = models.CharField(max_length=3, choices=settings.BRANCHES, default=settings.BRANCHES[0])
 	guardian_name = models.CharField(max_length=100)
 	phone_no = models.CharField(max_length=10)
-
-	@property
-	def registration_no(self):
-		return "S%05d" % self.id
 
 
 	def __str__(self):
 		return self.name
 
+@receiver(post_save, sender=Student)
+def update_student_reg_no(sender, instance, created, **kwargs):
+	if created:
+		instance.registration_no = "S%05d" % instance.id
+		instance.save()
+
+
 class Teacher(models.Model):
 	name = models.CharField(max_length=100)
+	registration_no = models.CharField(max_length=6, null=True, unique=True)
 	dob = models.DateField()
-	branch = models.CharField(max_length=3, choices=settings.BRANCHES)
+	branch = models.CharField(max_length=3, choices=settings.BRANCHES, default=settings.BRANCHES[0])
 	phone_no = models.CharField(max_length=10)
-
-	@property
-	def registration_no(self):
-		return "T%05d" % self.id
 	
 
 	def __str__(self):
 		return self.name
+
+@receiver(post_save, sender=Teacher)
+def update_teacher_reg_no(sender, instance, created, **kwargs):
+	if created:
+		instance.registration_no = "T%05d" % instance.id
+		instance.save()
--- credicxo_test/myapi/serializers.py
+++ credicxo_test/myapi/serializers.py
@@ -9,9 +9,15 @@ class StudentSerializer(serializers.HyperlinkedModelSerializer):
 		model = Student
 		fields = "__all__"
 
+	def create(self, validated_data):
+		return Student.objects.create(**validated_data)
+
 class TeacherSerializer(serializers.HyperlinkedModelSerializer):
 	registration_no = serializers.ReadOnlyField()
-	
+
 	class Meta:
 		model = Teacher
 		fields = "__all__"
+
+	def create(self, validated_data):
+		return Teacher.objects.create(**validated_data)
